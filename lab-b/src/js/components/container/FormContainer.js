e198d0a04c6079683fc7aed8bec6a589439d00c3
yumnyum <cm2spi@naver.com>
2018-10-31 22:49:59 +0900

    버튼이벤트

--- lab-b/src/js/components/container/FormContainer.js
+++ lab-b/src/js/components/container/FormContainer.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Input from "../presentational/Input";
+import TempTop from "../tempMain/TempTop";
+import Tempul from "../tempMain/Temp-ul";
+import axios from "axios";
+
+var getSiblings = function (elem) {
+	// Setup siblings array and get the first sibling
+	var siblings = [];
+	var sibling = elem.parentNode.firstChild;
+	// Loop through each sibling and push to the array
+	while (sibling) {
+		if (sibling.nodeType === 1 && sibling !== elem) {
+			siblings.push(sibling);
+		}
+		sibling = sibling.nextSibling
+	}
+	return siblings;
+};
+
 class FormContainer extends Component {
   constructor() {
     super();
     this.state = {
       //city: "SEOUL"
     };
-    this.handleChange = this.handleChange.bind(this);
+    //this.handleChange = this.handleChange.bind(this);
     this.changeTemp = this.changeTemp.bind(this);
     this.btn = [];
     // 서울:1835848 인천:1843564 창원:1846326 가평:1843082 교토:1857910 로스앤젤레스:5368381 뉴욕:5128638
@@ -19,38 +37,78 @@ class FormContainer extends Component {
     this.btn[5] = ['js-btn','5368381','로스앤젤레스'];
     this.btn[6] = ['js-btn','5128638','뉴욕'];
   }
-  handleChange(event) {
-    this.setState({ [event.target.id]: event.target.value });  
-  }
+  
   changeTemp(event){
-    //event.preventDefault();   
-    //event.target.getAttribute("data-city");
-    this.setState({[event.target.tempid]: event.target.getAttribute("data-city")});
-    console.log( this.state  );
+    event.preventDefault();    
+    let target = event.target.getAttribute("data-city");
+    let btn = event.target;    
+    getSiblings(btn).map((k,m)=>{ k.classList.remove('select'); });
+    btn.classList.add('select');
+    this.setTempData(target);
+    this.setTempUlData(target);
+  }
+  setTempData(target){
+    let resp;
+    let set = this;
+    axios.get('http://api.openweathermap.org/data/2.5/weather?id='+ target +'&appid=5cd86456a36ccfc630beca8372515a12').then(function (response){
+        resp = response.data; 
+        
+        Object.assign(set.setState, set.setState({
+            description: resp.weather[0].description,
+            main: Math.round(resp.main.temp- 273.15),
+            humidity: resp.main.humidity,
+            clouds: resp.clouds.all
+        }));
+         
+    });    
+  }  
+  setTempUlData(target){
+    let resp;
+    let set = this;
+    let arr = [];
+    let days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    axios.get('http://api.openweathermap.org/data/2.5/forecast?id='+ target +'&appid=5cd86456a36ccfc630beca8372515a12').then(function (response){
+        resp = response.data; 
+        for( let i = 3; i<resp.list.length; i=i+8){
+          let mins = i+4;
+          if( mins<resp.list.length ){
+            let nextday = resp.list[i].dt_txt.split(" ")[0];            
+            arr.push({ 
+              day: days[new Date(nextday).getDay()], 
+              main: resp.list[i].weather[0].main, 
+              max: Math.round(resp.list[i+2].main.temp - 273.15),
+              min: Math.round(resp.list[mins].main.temp - 273.15)
+            });
+          }          
+        }        
+        Object.assign(set.setState, set.setState({tempData:arr}));        
+    });
+  }
+  componentDidMount() {
+    this.setTempData(1835848);
+    this.setTempUlData(1835848);
+    
   }
   render() {    
-    const { city } = this.state;    
+    //const { city } = this.state; 
     return (
-      <form id="article-form">
-        <div className="form-group" >        
-          {/* <label htmlFor={"city"}>{"city"}</label>
-          <input 
-            className="form-control"
-            readOnly
-            text="city"
-            label="city"          
-            type="text"
-            id="city"
-            placeholder="SEOUL"
-          
-            // value={city}
-            // handleChange={this.handleChange}          
-          />         */}
-          {this.btn.map((c,i)=>{
-            return i === 0? (<button className="select" rel={c[0]} data-city={c[1]} onClick={this.changeTemp} >{c[2]}</button>):(<button rel={c[0]} data-city={c[1]} onClick={this.changeTemp} >{c[2]}</button>);
-          })}
+      <div>
+        <div className="temp-search">
+          <form id="article-form">
+            <div className="form-group" >          
+              {this.btn.map((c,i)=>{
+                return i === 0? (<button className="select" rel={c[0]} data-city={c[1]} onClick={this.changeTemp} >{c[2]}</button>):(<button rel={c[0]} data-city={c[1]} onClick={this.changeTemp} >{c[2]}</button>);
+              })}              
+            </div>
+          </form>          
+        </div>   
+        <div className="tempbox">
+          <TempTop description={this.state.description} main={this.state.main} humidity={this.state.humidity} clouds={this.state.clouds} />
+          <Tempul tempData={this.state.tempData} />
         </div>
-      </form>
+      </div>
+      
+      
     );
   }
 }
--- lab-b/src/js/components/tempMain/TempTop.js
+++ lab-b/src/js/components/tempMain/TempTop.js
@@ -2,46 +2,27 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
+
 class TempTop extends Component {
     constructor() {
-      super();
-      this.state = {
-        description: "",
-        main: "",
-        humidity: "",
-        clouds: ""
-      };
+      super();     
       
-    }    
-    componentDidMount() {
-        let resp;
-        let set = this;       
-        axios.get('http://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=5cd86456a36ccfc630beca8372515a12').then(function (response){
-            resp = response.data; 
-            set.setState({
-                description: resp.weather[0].description,
-                main: Math.round(resp.main.temp- 273.15),
-                humidity: resp.main.humidity,
-                clouds: resp.clouds.all
-            });
-            
-        });
-    }
+    }     
     
     render() {    
         return (
             <div className="main">
-                <h1> {this.state.description} </h1>
-                <div className="right"><span>{this.state.main}</span>°C</div>
+                <h1> {this.props.description} </h1>
+                <div className="right"><span>{this.props.main}</span>°C</div>
                 <ul>
-                    <li>humidity : <span>{this.state.humidity}</span>%</li>
-                    <li>clouds : <span>{this.state.clouds}</span>%</li>
+                    <li>humidity : <span>{this.props.humidity}</span>%</li>
+                    <li>clouds : <span>{this.props.clouds}</span>%</li>
                 </ul>
             </div>
         );
     }
 }
-const wrapper = document.getElementById("tempbox");
-wrapper ? ReactDOM.render(<TempTop />, wrapper) : false;
+//const wrapper = document.getElementById("tempbox");
+//wrapper ? ReactDOM.render(<TempTop description={""} main={""} humidity={""} clouds={""} />, wrapper) : false;
 
 export default TempTop;
