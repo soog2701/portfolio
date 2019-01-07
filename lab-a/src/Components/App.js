ff1be46c815ed5dae19005ba848c5dc48f3578ae
yumnyum <cm2spi@naver.com>
2019-01-07 23:49:51 +0900

    app 화면구성중

--- lab-a/src/Components/App.js
+++ lab-a/src/Components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { render } from 'react-dom';
 
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -7,17 +8,87 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-const styles = {
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import Typography from '@material-ui/core/Typography';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    appBar: {
+        marginLeft: drawerWidth,
+            [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+        },
+    },
     menuButton: {
         marginLeft: -12,
         marginRight: 20,
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
     },
-};
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+    },
+});
 class App extends React.Component {
+    state = {
+        mobileOpen: false,
+    };
+    handleDrawerToggle = () => {
+        this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+    };
     render() {
-        const { classes } = this.props;
-        return (
+        const { classes, theme } = this.props;
+        const drawer = (
             <div>
+                <div className={classes.toolbar} />
+                <Divider />
+                <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                    <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+                </List>
+                <Divider />
+                <List>
+                {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                    <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+                </List>
+            </div>
+        );
+        return (
+            <div className={classes.root}>
+                <CssBaseline />
                 <AppBar position="static">
                     <Toolbar>
                         <IconButton  color="inherit" aria-label="Menu">
@@ -25,7 +96,61 @@ class App extends React.Component {
                         </IconButton>
                     </Toolbar>
                 </AppBar>
-                <p>React here!</p>
+                <nav className={classes.drawer}>
+                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                <Hidden smUp implementation="css">
+                    <Drawer
+                    container={this.props.container}
+                    variant="temporary"
+                    anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+                    open={this.state.mobileOpen}
+                    onClose={this.handleDrawerToggle}
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                    >
+                    {drawer}
+                    </Drawer>
+                </Hidden>
+                <Hidden xsDown implementation="css">
+                    <Drawer
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                    variant="permanent"
+                    open
+                    >
+                    {drawer}
+                    </Drawer>
+                </Hidden>
+                </nav>
+                <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <Typography paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
+                    elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
+                    hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
+                    velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
+                    Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
+                    viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
+                    Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
+                    at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
+                    ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+                </Typography>
+                <Typography paragraph>
+                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus
+                    sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in.
+                    In hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
+                    sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo
+                    viverra maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
+                    ultrices sagittis orci a.
+                </Typography>
+                </main>
             </div>
         );
     }
@@ -47,5 +172,7 @@ class App extends React.Component {
 // };
 App.propTypes = {
     classes: PropTypes.object.isRequired,
+    container: PropTypes.object,
+    theme: PropTypes.object.isRequired,
 };
-export default withStyles(styles)(App);
+export default withStyles(styles, { withTheme: true })(App);
