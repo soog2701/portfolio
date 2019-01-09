889a7d6ebc33fa0a68bc39d928faf6c165a02c13
yumnyum <cm2spi@naver.com>
2019-01-09 21:51:49 +0900

    App 화면 구성중

--- lab-a/src/Components/App.js
+++ lab-a/src/Components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { render } from 'react-dom';
-
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -20,6 +20,9 @@ import ListItemText from '@material-ui/core/ListItemText';
 import MailIcon from '@material-ui/icons/Mail';
 import Typography from '@material-ui/core/Typography';
 
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
 const drawerWidth = 240;
 
 const styles = theme => ({
@@ -27,51 +30,81 @@ const styles = theme => ({
         display: 'flex',
     },
     drawer: {
-        [theme.breakpoints.up('sm')]: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
+        width: drawerWidth,
+        flexShrink: 0,
     },
     appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
         marginLeft: drawerWidth,
-            [theme.breakpoints.up('sm')]: {
-            width: `calc(100% - ${drawerWidth}px)`,
-        },
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
     },
     menuButton: {
-        marginLeft: -12,
+        marginLeft: 12,
         marginRight: 20,
-        [theme.breakpoints.up('sm')]: {
-            display: 'none',
-        },
+    },
+    hide: {
+        display: 'none',
     },
     toolbar: theme.mixins.toolbar,
     drawerPaper: {
         width: drawerWidth,
     },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
     content: {
         flexGrow: 1,
         padding: theme.spacing.unit * 3,
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
     },
 });
 class App extends React.Component {
+    // https://github.com/babel/babel/issues/8655 
+    // babelrc 참조. 아래 state 오류로인해 @babel/plugin-proposal-class-properties 설치
     state = {
-        mobileOpen: false,
+        open: false,
     };
-    handleDrawerToggle = () => {
-        this.setState(state => ({ mobileOpen: !state.mobileOpen }));
+    handleDrawerOpen = () => {
+        this.setState({ open: true });
+    };
+    handleDrawerClose = () => {
+        this.setState({ open: false });
     };
     render() {
         const { classes, theme } = this.props;
+        const { open } = this.state;
         const drawer = (
             <div>
-                <div className={classes.toolbar} />
                 <Divider />
                 <List>
                 {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
                     <ListItem button key={text}>
-                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                    <ListItemText primary={text} />
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
                     </ListItem>
                 ))}
                 </List>
@@ -89,67 +122,70 @@ class App extends React.Component {
         return (
             <div className={classes.root}>
                 <CssBaseline />
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton  color="inherit" aria-label="Menu">
+                <AppBar position="fixed" 
+                    className={classNames(classes.appBar, {
+                        [classes.appBarShift]: open,
+                    })}
+                >
+                    <Toolbar disableGutters={!open}>
+                        <IconButton  color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerOpen}
+                            className={classNames(classes.menuButton, open && classes.hide)}
+                        >
                             <MenuIcon />
                         </IconButton>
+                        <Typography variant="h6" color="inherit" noWrap>
+                            Persistent drawer
+                        </Typography>
                     </Toolbar>
                 </AppBar>
-                <nav className={classes.drawer}>
-                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                <Hidden smUp implementation="css">
-                    <Drawer
-                    container={this.props.container}
-                    variant="temporary"
-                    anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-                    open={this.state.mobileOpen}
-                    onClose={this.handleDrawerToggle}
+                <Drawer
+                    className={classes.drawer}
+                    variant="persistent"
+                    anchor="left"
+                    open={open}
                     classes={{
                         paper: classes.drawerPaper,
                     }}
-                    >
+                >
+                    <div className={classes.drawerHeader}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                        </IconButton>
+                    </div>
                     {drawer}
-                    </Drawer>
-                </Hidden>
-                <Hidden xsDown implementation="css">
-                    <Drawer
-                    classes={{
-                        paper: classes.drawerPaper,
-                    }}
-                    variant="permanent"
-                    open
-                    >
-                    {drawer}
-                    </Drawer>
-                </Hidden>
-                </nav>
-                <main className={classes.content}>
-                <div className={classes.toolbar} />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
-                    elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
-                    hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
-                    velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
-                    Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
-                    viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
-                    Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
-                    at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
-                    ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
-                </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus
-                    sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in.
-                    In hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
-                    sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo
-                    viverra maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-                    ultrices sagittis orci a.
-                </Typography>
+                </Drawer>
+                <main 
+                    className={classNames(classes.content, {
+                        [classes.contentShift]: open,
+                    })}
+                >
+                    <div className={classes.drawerHeader} />
+                    <Typography paragraph>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent
+                        elementum facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in
+                        hendrerit gravida rutrum quisque non tellus. Convallis convallis tellus id interdum
+                        velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing.
+                        Amet nisl suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod quis
+                        viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum leo.
+                        Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus
+                        at augue. At augue eget arcu dictum varius duis at consectetur lorem. Velit sed
+                        ullamcorper morbi tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+                    </Typography>
+                    <Typography paragraph>
+                        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
+                        facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
+                        tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
+                        consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus
+                        sed vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in.
+                        In hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+                        et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique
+                        sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo
+                        viverra maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
+                        ultrices sagittis orci a.
+                    </Typography>
                 </main>
             </div>
         );
