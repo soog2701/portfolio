4444eb26faa7cce8a5a937ef707b2e657fbfbc5f
yumnyum <cm2spi@naver.com>
2019-11-25 21:23:05 +0900

    메인 컨텐츠 작업중

--- lab-a/pages/index.js
+++ lab-a/pages/index.js
@@ -13,6 +13,8 @@ import { actionTypes, logoutRequest } from '../redux/module/login';
 
 // const token = cookies.get('token');
 
+import DesignInput from '../comps/DesignInput';
+
 import io from 'socket.io-client'
 
 function useSocket(url, payload) {
@@ -47,22 +49,89 @@ const Main = ({ t , click }) => (
                     </li>
                 </ul>
             </div>
-            <div className={styles['main-top--left']}>
+            <div className={styles['main-top--right']}>
                 현재 <span>{'432,440'}</span>개의 여행일정과 <span>{'480,650'}</span>개의 여행명소 정보가 있습니다.
             </div>
         </div>
         <div className={styles['main-middle']}>
             <div className={styles['main-middle--left']}>
-                <div className={styles['main-middle--slide']}></div>
+                <div className={styles['main-middle--slide']}>banner</div>
             </div>
             <div className={styles['main-middle--right']}>
                 <div className={styles['main-middle--air']}>
-
+                    <ul>
+                        <li>
+                            항공권조회
+                        </li>
+                    </ul>
+                    <div className={styles['main-middle--air-box']}>
+                        <label>
+                            <input type="radio" name="air" />
+                            왕복
+                        </label>
+                        <label>
+                            <input type="radio" name="air" />
+                            편도
+                        </label>
+                        <label>
+                            <input type="radio" name="air" />
+                            다구간
+                        </label>
+                    </div>
+                    <div className={styles['main-middle--air-box']}>
+                        <div>
+                            <span>출발지</span>
+                            <DesignInput />
+                        </div>
+                        <div style={{'marginLeft':'10px'}}>
+                            <span>도착지</span>
+                            <DesignInput />
+                        </div>
+                        
+                    </div>
+                    <div className={styles['main-middle--air-box']}>
+                        <div>
+                            <span>가는날</span>
+                            <DesignInput name="startday" />
+                        </div>
+                        
+                        <div style={{'marginLeft':'10px'}}>
+                            <span>귀국</span>
+                            <DesignInput name="endday" />
+                        </div>
+                        
+                    </div>
+                    <div className={styles['main-middle--air-box']}>
+                        <div>
+                            <span>성인</span>
+                            <select></select>
+                        </div>
+                        <div style={{'marginLeft':'10px'}}>
+                            <span>유소아</span>
+                            <select></select>
+                        </div>                        
+                    </div>
+                    <div className={styles['main-middle--air-box']}>
+                        <div>
+                            <span>좌석등급</span>
+                            <select></select>
+                        </div>
+                    </div>
+                    <div className={styles['main-middle--air-box']}>
+                        <button>항공권검색</button>
+                    </div>
                 </div>
             </div>
         </div>
         <div className={styles['main-bottom']}>
-
+            <ul>
+                <li>
+                    항공권조회
+                </li>
+            </ul>
+            <div>
+                content
+            </div>
         </div>
     </div>
 )
