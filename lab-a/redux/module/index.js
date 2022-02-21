3c9bd7d47945f2722bf4fe662f998d2fcfde8fe5
soog <cm2spi@naver.com>
2022-02-21 22:32:37 +0900

    delete all

--- lab-a/redux/module/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import login from './login';
-import exception from './exception';
-
-export default combineReducers({
-  login,
-  exception
-  // 다른 리듀서를 만들게되면 여기에 넣어줌..
-});
