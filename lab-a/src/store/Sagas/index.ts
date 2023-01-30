e8b9628a0d669ddadb5b6bb625d166928384ae9b
soog <cm2spi@naver.com>
2023-01-30 20:24:24 +0900

    delete lab folder

--- lab-a/src/store/Sagas/index.ts
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-
-export default function* rootSaga() {
-  yield all([ // 모든 saga 를 한번에 관리
-    // ...testSaga,
-  ]);
-}
--- lab-a/src/store/Type/news.ts
+++
@@ -1,4 +0,0 @@
-export const GETNEWS = "news/GETNEWS" as const;
-export const SETNEWS = "news/SETNEWS" as const;
-export const SAGA_LOAD_NEWS = "news/SAGA_LOAD_NEWS" as const;
-export const LOADNEWS = "news/LOADNEWS" as const;
